5a5376b8cded86bfd805eb45a056166a85d300de
Clement Pochart <clement.pochart01@gmail.com>
2025-10-28 07:27:48 +0100

    feat: version 2 (updated keybinds + tabs)

--- emacs-config.el
+++ emacs-config.el
@@ -1,28 +1,11 @@
 ;;
-;; clement.pochart and matthiru's
+;; poperinghe's emacs configuration
+;; https://github.com/Poperinghe/Dotfiles.git
 ;;         __
 ;;        / /__ _ __ ___   __ _  ___ ___
 ;;  /\/| / / _ \ '_ ` _ \ / _` |/ __/ __|
 ;; |/\/ / /  __/ | | | | | (_| | (__\__ \
 ;;     /_(_)___|_| |_| |_|\__,_|\___|___/
-
-
-;; SENSIBLE DEFAULTS
-;; -----------------
-
-(setq inhibit-startup-message t)                                ; no startup message
-(scroll-bar-mode -1)                                            ; disable the visible scrollbar
-(menu-bar-mode -1)                                              ; disable the menu-bar
-(tool-bar-mode -1)                                              ; disable the toolbar
-(tooltip-mode -1)                                               ; disable the tooltips
-(set-fringe-mode 5)                                             ; adds some margins
-(setq visible-bell t)                                           ; Visually show errors instead of beeping
-(setq gc-cons-threshold 50000000)                               ; Reduces the freq. of garbage collection
-(setq display-line-numbers-type 'relative)                      ; Changes line numbers to relative mode
-(add-hook 'prog-mode-hook 'display-line-numbers-mode)           ; Displays line numbers
-(setq backup-directory-alist `(("." . "~/.emacs.d/autosaves"))) ; Prevents autosave file clutter
-(setq scroll-step 1 scroll-margin 1)                            ; Scroll one line at a time
-
 
 ;; PACKAGE MANAGEMENT
 ;; ------------------
@@ -46,7 +29,6 @@
   :diminish :init
   (setq evil-want-integration t)
   (setq evil-want-keybinding nil)
-  (setq evil-want-C-u-scroll t)
   (setq evil-undo-system 'undo-redo)
   :config (evil-mode 1))
 
@@ -54,91 +36,144 @@
   :after evil
   :config (evil-collection-init))
 
-(when (bound-and-true-p evil-mode)
-  (define-key evil-normal-state-map (kbd "/") 'swiper))
+;; WINDOW MANAGEMENT
+;; -----------------
 
-(use-package avy)
+(setq inhibit-startup-message t)
+(tab-bar-mode 1)
+(scroll-bar-mode -1)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(setq visible-bell t)
+(setq display-line-numbers-type 'relative)
+(setq global-display-line-numbers-mode 1)
+(setq scroll-step 1 scroll-margin 1)
+(setq backup-directory-alist `(("." . "~/.emacs.d/autosaves")))
+(setq-default truncate-lines t)
 
-;; FONT AND THEME
-;; --------------
+(setq global-auto-revert-non-file-buffers t)
+(add-hook 'dired-mode-hook #'auto-revert-mode)
+(setq dired-listing-switches "-alh --group-directories-first")
 
-;; (set-face-attribute 'default nil :font "Mononoki Nerd Font Mono")
-(set-face-attribute 'default nil :height 140)
-(set-frame-parameter nil 'alpha-background 60)
-(add-to-list 'default-frame-alist '(alpha-background . 60))
+;; sync emacs env with shell env
+(use-package exec-path-from-shell
+  :ensure t
+  :config
+  (exec-path-from-shell-initialize))
 
-(use-package kaolin-themes
-  :config (load-theme 'kaolin-dark t))
+(use-package ibuffer)
 
+(dotimes (_ 3)
+  (tab-bar-new-tab))
 
-;; MINIBUFFER ENHANCEMENTS
-;; -----------------------
+;; KEYBINDS WITH GENERAL
+;; ---------------------
+
+(use-package general
+  :config
+  (general-evil-setup t)
+  (general-create-definer poperinghe/leader-key-map
+    :states '(normal visual emacs)
+    :keymaps 'override
+    :prefix "SPC"))
 
 (use-package ivy
   :diminish
-  :bind (:map ivy-minibuffer-map
-         ("C-l" . ivy-alt-done)
-         ("C-j" . ivy-next-line)
-         ("C-k" . ivy-previous-line)
-         :map ivy-switch-buffer-map
-         ("C-k" . ivy-previous-line)
-         ("C-l" . ivy-done)
-         ("C-d" . ivy-switch-buffer-kill)
-         :map ivy-reverse-i-search-map
-         ("C-k" . ivy-previous-line)
-         ("C-d" . ivy-reverse-i-search-kill))
-  :config
-  (ivy-mode 1))
+  :config (ivy-mode 1))
 
 (use-package counsel)
 
+(general-define-key
+ :keymaps 'evil-normal-state-map
+ "/" 'swiper)
 
-(use-package which-key
-  :init (which-key-mode)
-  :diminish which-key-mode)
+(general-define-key
+ :keymaps 'ivy-minibuffer-map
+ "C-j" 'ivy-next-line
+ "C-k" 'ivy-previous-line)
+
+(general-define-key
+ :keymaps 'ibuffer-mode-map
+ :states '(normal)
+ "l" 'ibuffer-visit-buffer)
+
+(general-define-key
+ :keymaps 'dired-mode-map
+ :states '(normal)
+ "h" 'dired-up-directory
+ "l" 'dired-find-file)
+
+(general-define-key
+ :keymaps 'override
+ :states '(normal visual insert emacs)
+ "M-Q" 'delete-window
+ "M-q" 'kill-buffer
+ "M-RET" '(lambda () (interactive) (select-window (split-window-right)))
+ "M-;" '(lambda () (interactive) (select-window (split-window-below)))
+ "M-1" '(lambda () (interactive) (tab-bar-select-tab 1))
+ "M-2" '(lambda () (interactive) (tab-bar-select-tab 2))
+ "M-3" '(lambda () (interactive) (tab-bar-select-tab 3))
+ "M-4" '(lambda () (interactive) (tab-bar-select-tab 4))
+ "C-u" '(lambda () (interactive) (scroll-down-command 5))
+ "C-d" '(lambda () (interactive) (scroll-up-command 5))
+ "M-h" 'windmove-left
+ "M-j" 'windmove-down
+ "M-k" 'windmove-up
+ "M-l" 'windmove-right)
+
+(poperinghe/leader-key-map
+  ;; FIND KEYBINDINGS ;;
+  "f" '(:ignore t :which-key "find")
+  "ff" '(find-file :which-key "find file")
+  "fi" '((lambda () (interactive)(find-file "~/.emacs")) :which-key "find emacs-config")
+  "fs" '(scratch-buffer :which-key "find scratch buffer")
+
+  ;; EVAL/EXECUTE KEYBINDINGS ;;
+  "e" '(:ignore t :which-key "eval/execute")
+  "eb" '(eval-buffer t :which-key "eval buffer")
+  "ej" '(execute-extended-command :which-key "m-x")
+
+  ;; BUFFER NAVIGATION KEYBINDINGS ;;
+  "u" '(ibuffer :which-key "buffers")
+)
 
 
-;; CODE FORMATTING
-;; ---------------
 
-(setq c-default-style '((c-mode . "awk") ;; default style close from
-                        (c++-mode . "awk") ;; the coding style
-                        (java-mode . "java") ;; the coding style
-                        (other . "awk")))
+;; ------------------------------------------------------------------
+;;                     IDE SETTINGS BEYOND THIS POINT
+;; ------------------------------------------------------------------
 
-;; DIRED
-;; -----
 
-(with-eval-after-load 'dired
-  (evil-define-key 'normal dired-mode-map
-    "h" 'dired-up-directory
-    "l" 'dired-find-file
-    " " 'nil))
+
+(use-package doom-modeline
+  :config (doom-modeline-mode 1))
+
+;;(use-package doom-themes
+  ;;:config (load-theme 'doom-horizon))
+
+(use-package kaolin-themes
+  :config (load-theme 'kaolin-dark))
+  ;;:config (load-theme 'kaolin-galaxy))
+
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
 
 (use-package magit)
-
-;; PROJECTILE
-;; ----------
-
 (use-package projectile
-  :init
-  (setq projectile-project-search-path '("~/Files/"))
-  :config
-  (global-set-key (kbd "C-c p") 'projectile-command-map)
-  (projectile-mode +1))
-
-(use-package counsel-projectile
- :after projectile
- :config (counsel-projectile-mode 1))
-
-
-;; LSP 
-;; ---
+  :init (setq projectile-project-search-path '("~/Files/"))
+  :config (projectile-mode +1))
+(use-package counsel-projectile)
 
 (use-package lsp-mode
-  :hook (java-mode . lsp-mode))
+  :config
+  (setq lsp-completion-show-kind nil)
+  (setq lsp-keep-workspace-alive nil)
+  (setq lsp-format-buffer-on-save-list '(c-mode c++-mode))
+  (setq lsp-format-buffer-on-save t))
 
-(use-package lsp-ui :commands lsp-ui-mode)
+(use-package lsp-ui
+  :commands lsp-ui-mode
+  :config
+  (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions))
 
 (use-package flycheck)
 (use-package flycheck-pos-tip
@@ -148,116 +183,80 @@
   :hook (prog-mode . company-mode)
   :config
   (define-key company-active-map (kbd "<tab>") 'company-complete-selection)
+  (setq company-tooltip-align-annotations t)
   (setq company-minimum-prefix-length 1)
   (setq company-idle-delay 0))
 
+(defun poperinghe/lsp-stop ()
+  "Disconnect all clangd-related LSP workspaces for the current project and kill their processes."
+  (interactive)
+  (let ((lsp-keep-workspace-alive nil)
+        (project-root (lsp-workspace-root)))
+    (dolist (ws (lsp-workspaces))
+      (when (string-prefix-p project-root (lsp--workspace-root ws))
+        (lsp-workspace-shutdown ws)))
+    (lsp-mode -1)
+    (flycheck-mode -1)
+    (message "Disconnected all clangd workspaces for project: %s" project-root)))
+
+(defun poperinghe/man-page-at-point ()
+  "open man page for the symbol at point."
+  (interactive)
+  (let ((sym (thing-at-point 'symbol t)))
+    (when sym (man sym))))
+
+(defun poperinghe/lsp-start-or-restart ()
+  "starts the language server or restarts it if it is already active."
+  (interactive)
+  (if (bound-and-true-p lsp--cur-workspace)
+      (lsp-restart-workspace)
+    (lsp)))
+
+(defun poperinghe/magit-log-all-graph ()
+  "Show Magit log with --graph --decorate --all."
+  (interactive)
+  (magit-log '("HEAD") '("--graph" "--decorate" "--all")))
+
+(poperinghe/leader-key-map
+  ;; LSP KEYBINDINGS ;;
+  "l" '(:ignore t :which-key "lsp")
+  "lp" '(:ignore t :which-key "lsp peek WIP")
+  "ls" '(poperinghe/lsp-start-or-restart :which-key "lsp start or restart")
+  "lk" '(poperinghe/lsp-stop :which-key "lsp kill")
+  "lm" '(poperinghe/man-page-at-point :which-key "lsp man-page")
+  "ln" '(flycheck-next-error :which-key "lsp next error")
+  "lr" '(lsp-rename :which-key "lsp rename")
+  "lf" '(:ignore t :which-key "lsp find")
+  "lfd" '(lsp-find-definition :which-key "lsp find definition")
+  "lfr" '(lsp-find-references :which-key "lsp find references")
+
+  ;; PROJECTILE KEYBINDINGS ;;
+  "p" '(:ignore t :which-key "projectile")
+  "pg" '(projectile-grep :which-key "projectile grep")
+  "ps" '(projectile-switch-project :which-key "projectile switch (switch project)")
+  "pf" '(projectile-find-file :which-key "projectile find")
+
+  ;; MAGIT KEYBINDINGS ;;
+  "g" '(:ignore t :which-key "git (magit)")
+  "gs" '(magit :which-key "git status")
+  "gl" '(poperinghe/magit-log-all-graph :which-key "git log")
+  )
+
 (use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
-
-
-;; GENERAL AND HYDRA
-;; -----------------
-
-(use-package general
-  :config
-  (general-evil-setup t)
-
-  (general-create-definer rune/leader-keys
-    :keymaps '(normal visual emacs)
-    :prefix "SPC"))
-
-
-(rune/leader-keys
-  "k"  '(:ignore t :which-key "orgmode")
-  "kk"  '(org-todo :which-key "org-todo-cycle")
-  "ko"  '(org-open-at-point :which-key "org-open-link"))
-
-(rune/leader-keys
-  "d"  '((lambda () (interactive) (dired default-directory)) :which-key "dired"))
-
-(rune/leader-keys
-  "g" '(magit :which-key "magit"))
-
-(rune/leader-keys
-  "e"  '(:ignore t :which-key "execute")
-  "ej" '(counsel-M-x :which-key "M-x"))
-
-(rune/leader-keys
-  "t"  '(:ignore t :which-key "toggles")
-  "ts" '(hydra-text-scale/body :which-key "scale text")
-  "ta" '(hydra-all-switch-theme/body :which-key "toggle ALL themes")
-  "tt" '(hydra-switch-theme/body :which-key "toggle favorite theme"))
-
-(rune/leader-keys
-  "f"  '(:ignore t :which-key "find")
-  "ff" '(find-file :which-key "find file")
-  "fs" '(save-buffer :which-key "file save")
-  "fh" '((lambda () (interactive) (switch-to-buffer "*scratch*")) :which-key "scratch")
-  "fi" '((lambda() (interactive)(find-file "~/.emacs")) :which-key "~/.emacs"))
-
-(rune/leader-keys
-  "j"  '(:ignore t :which-key "jump")
-  "jl"  '(avy-goto-line :which-key "avy go to line")
-  "ja"  '(avy-goto-word-1 :which-key "avy go to word 1"))
-
-(rune/leader-keys
-  "b"  '(:ignore t :which-key "buffer")
-  "by" '(copy-whole-buffer :which-key "yank buffer")
-  "bl" '(ibuffer :which-key "buffer list")
-  "be" '(eval-buffer :which-key "eval buffer")
-  "bj" '(next-buffer :which-key "next")
-  "bd" '(kill-this-buffer :which-key "kill buffer")
-  "bk" '(previous-buffer :which-key "previous"))
-
-(rune/leader-keys
-  "p"  '(:ignore t :which-key "projectile")
-  "pp" '(projectile-switch-project :which-key "switch project")
-  "pa" '(projectile-find-other-file :which-key "find other file")
-  "pg" '(counsel-projectile-grep :which-key "grep")
-  "pd" '(projectile-discover-projects-in-search-path :which-key "discover projects")
-  "pf" '(projectile-find-file :which-key "find file in project"))
-
-(rune/leader-keys
-  "l"  '(:ignore t :which-key "lsp")
-  "lr" '(lsp-rename :which-key "rename")
-  "ls" '(lsp-workspace-restart :which-key "restart lsp")
-  "la" '(lsp-execute-code-action :which-key "code action")
-  "ln" '(flycheck-next-error :which-key "next error")
-  "lm" '(:ignore t :which-key "maven")
-  "lma" '(maven-test-all t :which-key "run all tests")
-  "ld" '(:ignore r :which-key "doc")
-  "lds" '(lsp-ui-doc-show :which-key "doc show")
-  "ldh" '(lsp-ui-doc-hide :which-key "doc hide"))
-
-
-(rune/leader-keys
-  "w"  '(:ignore t :which-key "windows")
-  "wv" '(split-window-horizontally :which-key "split window vertically")
-  "wh" '(split-window-vertically :which-key "split window horizontally")
-  "wt" '(toggle-transparency :which-key "split window horizontally")
-  "wd" '(delete-window :which-keh "close current window"))
-
-
-
-
-
-
-
-
-
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("9d5a33a0097c43f44759530c846e1adf9c40171f232a4b2ae561feccc99a03c4"
-     "5291b60ee27dfc12078f787929498ce82efe5e4d42decdbb994be80cdb2def1f"
-     "c341518f5a80752f3113699a7f845dfc7299667311858e7cdfe64677d359d87e"
-     "5beb9cc517b24959e2ee7be47584270bbe11a7b210807fa419d41ede12174a26"
-     "ca2ce81d33e0b4bd0fdf20caefdde9cb617fec42eeeaf5cd79c80d630bd5cf6a"
+   '("5291b60ee27dfc12078f787929498ce82efe5e4d42decdbb994be80cdb2def1f"
+     "4990532659bb6a285fee01ede3dfa1b1bdf302c5c3c8de9fad9b6bc63a9252f7"
+     "3538194fff1b928df280dc08f041518a8d51ac3ff704c5e46d1517f5c4d8a0e0"
      default))
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(company counsel-projectile doom-modeline doom-themes evil-collection
+	     exec-path-from-shell flycheck-pos-tip general
+	     kaolin-themes lsp-ivy lsp-ui magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
