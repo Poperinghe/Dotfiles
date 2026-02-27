4ec2beffcd6f7285681a5ab0d625d9433faa1a21
Clement Pochart <clement.pochart01@gmail.com>
2026-02-27 18:21:41 +0100

    feat: flake for emacs startup

--- .bashrc
+++ .bashrc
@@ -1,4 +1,5 @@
 . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
 PS1='\[\e[38;5;245m\][\u@\h:\W]\\$ \[\e[0m\]'
 
-alias e="emacs -nw"
+alias e="sudo emacs -nw -q -l ~/.emacs"
+alias dev="nix develop --extra-experimental-features nix-command --extra-experimental-features flakes"
---
+++ emacs/flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Emacs nix environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # or stable
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            cmake
+            gnumake
+            libtool
+          ];
+        };
+      });
+}
